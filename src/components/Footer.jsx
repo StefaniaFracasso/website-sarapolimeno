06adb488b3e9a4051dc97bb96c0f007e1f79c74e
StefaniaFracasso <122379841+StefaniaFracasso@users.noreply.github.com>
2023-08-31 16:05:33 +0200

    modifiche link footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,72 +1,5 @@
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-// import { faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons'
-
-// function Footer() {
-//   return (
-//     <footer className="relative bg-customGreen pt-8 pb-6">
-//       <div className="container mx-auto px-4">
-//         <div className="flex flex-wrap text-left lg:text-left">
-//           <div className="w-full lg:w-6/12 px-4">
-//             <h4 className="text-3xl font-marteni text-white">Sara Anna Polimeno</h4>
-//             <h5 className="text-lg font-agrandirmt-0 mb-2 text-white">
-//               Biologo Nutrizionista
-//             </h5>
-//             <div className="mt-6 lg:mb-0 mb-6">
-//                 <p className='text-white mb-2'>Seguimi sui social:</p>
-//               <button className="bg-white text-lightBlue-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2 hover:scale-125 transition duration-150 ease-in-out hover:text-customPeach" type="button">
-//                 <a href="https://www.facebook.com/profile.php?id=100063472213879" target='_blank' rel="noreferrer"><FontAwesomeIcon icon={faFacebook} /></a>
-//               </button>
-//               <button className="bg-white text-lightBlue-600 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2 hover:scale-125 transition duration-150 ease-in-out hover:text-customPeach" type="button">
-//               <a href="instagram://user?username=saraanna_nutrizionista"><FontAwesomeIcon icon={faInstagram}/></a>
-//               </button>
-//             </div>
-//           </div>
-//           <div className="lg:mb-0">
-//                 <p className='text-white mb-2'>Seguimi sui social:</p>
-//               <button className="bg-white text-lightBlue-400 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2 hover:scale-125 transition duration-150 ease-in-out hover:text-customPeach" type="button">
-//                 <a href="https://www.facebook.com/profile.php?id=100063472213879" target='_blank' rel="noreferrer"><FontAwesomeIcon icon={faFacebook} /></a>
-//               </button>
-//               <button className="bg-white text-lightBlue-600 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2 hover:scale-125 transition duration-150 ease-in-out hover:text-customPeach" type="button">
-//               <a href="instagram://user?username=saraanna_nutrizionista"><FontAwesomeIcon icon={faInstagram}/></a>
-//               </button>
-//             </div>
-//           <div className="w-full lg:w-6/12 px-4">
-//             <div className="flex flex-wrap items-top mb-6">
-//               <div className="w-full lg:w-4/12 px-4 ml-auto">
-//                 <span className="block uppercase text-blueGray-500 text-sm font-semibold mb-2 text-white">Useful Links</span>
-//                 <ul className="list-unstyled">
-//                   {/* Links here */}
-//                 </ul>
-//               </div>
-//               <div className="w-full lg:w-4/12 px-4">
-//                 <span className="block uppercase text-blueGray-500 text-sm font-semibold mb-2 text-white">Other Resources</span>
-//                 <ul className="list-unstyled">
-//                   {/* Links here */}
-//                 </ul>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//         <hr className="my-6 border-white" />
-//         <div className="flex flex-wrap items-center md:justify-between justify-center">
-//           <div className="w-full md:w-4/12 px-4 mx-auto text-center">
-//             <div className="text-sm text-white font-semibold py-1">
-//             {new Date().getFullYear()} | Copyright ©&nbsp;
-//               <a href="https://www.creative-tim.com/product/notus-js" className="text-blueGray-500 italic hover:text-customPeach" target="_blank" rel="noreferrer">
-//                 Stefania Fracasso</a>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </footer>
-//   );
-// }
-
-// export default Footer;
-
-
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faFacebook, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 function Footer() {
   return (
@@ -90,21 +23,22 @@ function Footer() {
                   <FontAwesomeIcon icon={faInstagram} />
                 </a>
               </button>
+              <button className="bg-white text-lightBlue-600 shadow-lg font-normal h-10 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2 hover:scale-125 transition duration-150 ease-in-out hover:text-customPeach" type="button">
+                <a href="https://www.linkedin.com/in/sara-anna-polimeno-29691994" target='_blank' rel='noreferrer'>
+                  <FontAwesomeIcon icon={faLinkedin} />
+                </a>
+              </button>
             </div>
           </div>
           <div className="hidden md:block w-full lg:w-4/12 px-4">
             <div className="flex flex-wrap items-top mb-6 justify-center lg:justify-end">
               <div className="w-full lg:w-auto px-4 ml-auto">
-                <span className="block uppercase text-blueGray-500 text-sm font-semibold mb-2 text-white">Useful Links</span>
-                <ul className="list-unstyled">
-                  {/* Links here */}
-                </ul>
+                <a className="block uppercase text-blueGray-500 text-sm font-semibold mb-2 text-white hover:text-customPeach cursor-pointer">Chi sono</a>
+                <a className="block uppercase text-blueGray-500 text-sm font-semibold mb-2 text-white hover:text-customPeach cursor-pointer">Servizi</a>
               </div>
               <div className="w-full lg:w-auto px-4">
-                <span className="block uppercase text-blueGray-500 text-sm font-semibold mb-2 text-white">Other Resources</span>
-                <ul className="list-unstyled">
-                  {/* Links here */}
-                </ul>
+                <a className="block uppercase text-blueGray-500 text-sm font-semibold mb-2 text-white hover:text-customPeach cursor-pointer">La prima visita</a>
+                <a className="block uppercase text-blueGray-500 text-sm font-semibold mb-2 text-white hover:text-customPeach cursor-pointer">Consigli nutrizionali</a>
               </div>
             </div>
           </div>
@@ -114,7 +48,7 @@ function Footer() {
           <div className="w-full md:w-4/12 px-4 mx-auto text-center">
             <div className="text-sm text-white font-semibold py-1">
               {new Date().getFullYear()} | Copyright ©&nbsp;
-              <a href="https://www.creative-tim.com/product/notus-js" className="text-blueGray-500 italic hover:text-customPeach" target="_blank" rel="noreferrer">
+              <a href="https://stefaniafracasso.netlify.app/" className="text-blueGray-500 italic hover:text-customPeach" target="_blank" rel="noreferrer">
                 Stefania Fracasso
               </a>
             </div>
